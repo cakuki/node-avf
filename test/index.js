370a9a7278234a0b19fa7f4d6490beb80e084934
Can Kinay <me@ckk.im>
2023-11-15 00:39:57 +0100

    refactor: use internal test lib

--- test/index.js
+++ test/index.js
@@ -1,9 +1,25 @@
+import { describe, it } from "node:test";
+import assert from "node:assert";
+import fs from "node:fs/promises";
+
 import { trim } from "../avf.js";
 
-async function test() {
-  const input = "test.mp4";
-  const output = "test.output.mp4";
-  await trim(input, output, "00:00:00", "00:00:05");
-}
+describe("trim", () => {
+  it("should trim", async () => {
+    const input = "test.mp4";
+    const output = "test.output.mov";
 
-await test();
+    await trim(input, output, 0, 0);
+
+    // Check the output file exists
+    const exists = await fs
+      .access(output, fs.constants.F_OK)
+      .then(() => true)
+      .catch(() => false);
+    assert(exists);
+
+    // Check the output file is not empty
+    const stat = await fs.promises.stat(output);
+    assert.notEqual(stat.size, 0);
+  });
+});
